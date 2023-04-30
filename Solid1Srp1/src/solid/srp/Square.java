768de85a794e6a471d8bfec92dc08badf93a10c0
Pavel <black_yaguar@mail.ru>
2023-04-30 11:29:36 +0500

    решение первой задачи

--- Solid1Srp1/src/solid/srp/Square.java
+++ Solid1Srp1/src/solid/srp/Square.java
@@ -7,9 +7,15 @@ public class Square {
 
     private List<Point> points;
     private int side;
+    private int zoom=1;
+
+    public void setZoom(int zoom) {
+        this.zoom = zoom;
+        this.side*=zoom;
+    }
 
     public Square(Point point1, int side) {
-        this.side = side;
+        this.side = side*zoom;
 
         Point point2 = new Point(point1.getX() + side, point1.getY());
         Point point3 = new Point(point1.getX() + side, point1.getY() + side);
@@ -26,24 +32,26 @@ public class Square {
         return points;
     }
 
-    public int getArea() {
-        return side * side;
-    }
+    
 
     public void draw() {
         for (int i = 0; i < side; i++) {
-            System.out.print("*");
+            System.out.print("* ");
         }
         System.out.println();
-        for (int i = 0; i < side; i++) {
-            System.out.print("*");
+        for (int i = 0; i < side-2; i++) {
+            System.out.print("* ");
             for (int j = 1; j < side - 1; j++) {
-                System.out.print(" ");
+                System.out.print("  ");
             }
-            System.out.println("*");
+            System.out.println("* ");
         }
         for (int i = 0; i < side; i++) {
-            System.out.print("*");
+            System.out.print("* ");
         }
+    }
+
+    public int getSide() {
+        return side;
     }
 }
