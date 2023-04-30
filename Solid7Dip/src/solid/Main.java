099a8b77150ad66c81c2b66d86456892d4ff447b
Pavel <black_yaguar@mail.ru>
2023-05-01 00:31:16 +0500

    Седьмая задача

--- Solid7Dip/src/solid/Main.java
+++ Solid7Dip/src/solid/Main.java
@@ -1,11 +1,13 @@
 package solid;
 
+import solid.dip.PrintReport;
 import solid.dip.Report;
+import solid.dip.Reportable;
 
 public class Main {
     public static void main(String[] args) {
         Report report = new Report();
         report.calculate();
-        report.output();
+        report.output(new PrintReport() );
     }
 }
---
+++ Solid7Dip/src/solid/dip/Reportable.java
@@ -0,0 +1,8 @@
+package solid.dip;
+
+import java.util.List;
+
+public interface Reportable { 
+    void output(List<ReportItem> items);
+    
+}
