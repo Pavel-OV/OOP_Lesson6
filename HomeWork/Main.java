efaa1ada1439dd05fa001ae44dd80b3a0ff4b60c
Pavel <black_yaguar@mail.ru>
2023-05-01 10:56:32 +0500

    Седьмая задача

--- HomeWork/Main.java
+++ HomeWork/Main.java
@@ -1,7 +1,10 @@
+
 public class Main{
 	public static void main(String[] args){
 		User user = new User("Bob");
-		user.report();
-		user.save();
+		Persister users = new Persister(user);
+		Report report = new Report();
+		report.report( user);
+		users.save();
 	}
 }
